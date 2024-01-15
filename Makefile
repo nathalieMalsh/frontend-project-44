ad32ecc7bf7bf9fce5475fc866121035f672dca9
nmalysheva99@mail.ru <nathalie_malsh@LAPTOP-HHAP2AP9>
2024-01-15 19:27:13 +0300

    add .eslintrc.yml

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ brain-games: #запуск игры
 
 publish: #отладка публикации
 	npm publish --dry-run
+
+lint: #запуск линтера
+	npx eslint .
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
-
 import greeting from '../src/cli.js';
 
-const helloToUser = greeting();
+console.log('Welcome to the Brain Games!');
 
-
-
+console.log(greeting());
