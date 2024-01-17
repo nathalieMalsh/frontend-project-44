5ad693928877fc17b6b069b0e8ff44ebf73e9f11
nmalysheva99@mail.ru <nathalie_malsh@LAPTOP-HHAP2AP9>
2024-01-17 21:01:40 +0300

    add brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,36 +1,3 @@
-import readlineSync from 'readline-sync';
-
-const isEven = number => (number % 2 === 0);
-
-const getNumber = () => {
-  const randomNumber = Math.floor(Math.random()*101);
-  return randomNumber;
-};
-
-const round = () => {
-  const numberForGame = getNumber();
-  const correctAnswer = (isEven(numberForGame)) ? 'yes' : 'no';
-  const userAnswer = readlineSync.question(`Answer "yes" if the number is even, otherwise answer "no". Question: ${numberForGame} Your answer: `);
-  
-  if (correctAnswer === userAnswer) {
-    console.log('Correct!');
-    return true;
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. `);
-    return false;
-  };
-};
-
-const playGame = () => {
-const userName = readlineSync.question('May I have your name? ');
-  console.log('Hello, ' + userName + '!');
-  for (let i = 0; i < 3; i++) {
-    const isCorrect = round();
-    if (!isCorrect) {
-      return;
-    };
-  };
-  console.log(`Congratulations, ${userName}!`);
-};
+import {isEven, getNumber, round, playGame} from '../src/even.js'
 
 playGame();
---
+++ src/even.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+
+export const isEven = number => (number % 2 === 0);
+
+export const getNumber = () => {
+  const randomNumber = Math.floor(Math.random()*101);
+  return randomNumber;
+};
+
+export const round = () => {
+  const numberForGame = getNumber();
+  const correctAnswer = (isEven(numberForGame)) ? 'yes' : 'no';
+  const userAnswer = readlineSync.question(`Answer "yes" if the number is even, otherwise answer "no". Question: ${numberForGame} Your answer: `);
+
+  if (correctAnswer === userAnswer) {
+    console.log('Correct!');
+    return true;
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. `);
+    return false;
+  };
+};
+
+export const playGame = () => {
+const userName = readlineSync.question('May I have your name? ');
+  console.log('Hello, ' + userName + '!');
+  for (let i = 0; i < 3; i++) {
+    const isCorrect = round();
+    if (!isCorrect) {
+      return;
+    };
+  };
+  console.log(`Congratulations, ${userName}!`);
+};
+
